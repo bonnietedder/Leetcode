c4c089fa215b6371b70c746457b5343bb5d4587e
bonnietedder <yingfeichen@vivaldi.net>
2019-11-20 22:44:22 -0500

    LongestPalindrome

--- src/leetcode/lc409.java
+++ src/leetcode/lc409.java
@@ -1,4 +1,30 @@
 package leetcode;
 
 public class lc409 {
+
+    public int LongestPalindrome(String s){
+        int len = 0;
+        boolean[] map = new boolean[128];
+        for(char c : s.toCharArray()){
+            map[c] = !map[c];
+            if (!map[c]) {
+                len += 2;
+            }
+        }
+        if(len < s.length()) {
+            len += 1;
+        }
+        return len;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abccccdd";
+        String s2 = "aabbccdd";
+        String s3 = "abcdee";
+        lc409 solution = new lc409();
+        System.out.println(solution.LongestPalindrome(s1));
+        System.out.println(solution.LongestPalindrome(s2));
+        System.out.println(solution.LongestPalindrome(s3));
+    }
+
 }
---
+++ src/leetcode/test.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class test {
+    public static void main(String[] args) {
+        int[] array =new int []{1,2,3};
+        System.out.println(Arrays.toString(array));
+        int[] cnt = new int[6];
+        System.out.println(Arrays.toString(cnt));
+        boolean[] map = new boolean[10];
+        System.out.println(Arrays.toString(map));
+    }
+}
+
+
