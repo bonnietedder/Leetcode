7af596a6242da16d34cfe54897c1ccf6b9dfc842
bonnietedder <yingfeichen@vivaldi.net>
2019-11-22 01:41:10 -0500

    greedy algorithm

---
+++ src/leetcode/lc455.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class lc455 {
+    public static void main(String[] args) {
+        lc455 solution = new lc455();
+        int[] children = new int[]{4,3,2,1};
+        int[] cookies = new int[]{1,3,5,7};
+        System.out.println(solution.findCotentChildren(children,cookies));
+    }
+
+    public int findCotentChildren (int[] children,int[] cookies){
+        Arrays.sort(children);
+        Arrays.sort(cookies);
+        int i =0, j= 0;
+        while(i<children.length && j<cookies.length){
+            if(children[i] <= cookies[j]) ++i;
+            j++;
+        }
+        return i;
+    }
+
+}
